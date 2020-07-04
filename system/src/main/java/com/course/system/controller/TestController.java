a51a98dd2587cc1b002dd578db2ace8f881c1894
Jackzhar <2315927859@qq.com>
2020-07-04 14:16:18 +0800

    搭建springCloud框架，并组件路由服务

---
+++ system/src/main/java/com/course/system/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.course.system.controller;
+
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableEurekaClient
+public class TestController {
+
+    @RequestMapping("/test")
+    public  String test(){
+        return "success";
+    }
+}
