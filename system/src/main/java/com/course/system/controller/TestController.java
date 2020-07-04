2c1ad9086346bbc94bdccfaaa21b508c1de73028
Jackzhar <2315927859@qq.com>
2020-07-04 17:38:51 +0800

    更新并链接mysql查询数据成功

--- system/src/main/java/com/course/system/controller/TestController.java
+++ system/src/main/java/com/course/system/controller/TestController.java
@@ -1,15 +1,30 @@
 package com.course.system.controller;
 
+import com.course.system.domain.Test;
+import com.course.system.service.TestService;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+import java.util.List;
+
 @RestController
-@EnableEurekaClient
+@ComponentScan("com.course")
+@MapperScan("com.course.system.mapper")
 public class TestController {
 
-    @RequestMapping("/test")
+   /* @RequestMapping("/test")
     public  String test(){
         return "success";
+    }*/
+
+    @Resource
+    private TestService testService;
+    @RequestMapping("/test")
+    public List<Test> test(){
+        return  testService.list();
     }
 }
